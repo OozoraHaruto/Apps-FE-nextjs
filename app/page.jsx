ce67979fe186a9ae6021cec14e8c0ab1a1c5f9cf
春音 <chibi.taiyoozora@gmail.com>
2025-03-19 19:37:50 +0800

    ADD: more info in the index page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import NavBar from './components/NavBar';
 
 export const metadata = {
@@ -13,9 +15,22 @@ export default function Home() {
       <main className="centerbox">
         <wa-card className="card-basic">
           <div style={ { fontSize: "var(--wa-font-size-4xl)" } }>Hi,</div>
-          <p>
-            Welcome, this website is still in a testing phase.
-          </p>
+          <div>
+            Welcome, this website and it's framework I'm using is still in a testing phase.
+            <br />
+            Do look around, if you found any issues please let me know
+          </div>
+          <wa-divider></wa-divider>
+          <div>
+            If you are a recruiter please look at&nbsp;
+            <Link href="/portfolio">
+              my portfolio
+            </Link>
+            .<br />
+            If you intend to call me please drop me a whatsapp/text first.
+            <br />
+            Due to the amount of spam calls nowadays I rarely pick up cold calls.
+          </div>
         </wa-card>
       </main>
     </>
