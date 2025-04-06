9fe124a9df0fdd305669cc1a86878bd83080b568
春音 <chibi.taiyoozora@gmail.com>
2025-04-06 14:45:15 +0800

    ADD: cloudinary

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   crossOrigin: 'anonymous',
   env: {
     AUTH_TOKEN: process.env.AUTH_TOKEN,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_UPLOAD_PRESET: process.env.NEXT_PUBLIC_UPLOAD_PRESET,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
   },
   experimental: {
     inlineCss: true,
